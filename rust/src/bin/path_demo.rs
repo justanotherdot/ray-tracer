e0c9fb888e5cd7eed7b7f81420ba64bcf033404c
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-05-22 16:34:18 +1000

    Plot demo from ch01

--- rust/src/bin/path_demo.rs
+++ rust/src/bin/path_demo.rs
@@ -1,4 +1,9 @@
+use rust_ray_tracer::canvas::Canvas;
+use rust_ray_tracer::color::Color;
 use rust_ray_tracer::coordinate::{Point, Vector};
+use rust_ray_tracer::ppm::Ppm;
+use std::fs::File;
+use std::io::prelude::*;
 
 struct Projectile {
     position: Point,
@@ -16,26 +21,36 @@ fn tick(env: &Environment, proj: Projectile) -> Projectile {
     Projectile { position, velocity }
 }
 
-fn simulate() {
+fn simulate_and_plot() -> Ppm {
     let position = Point::new(0., 1., 0.);
-    let velocity = Vector::new(1., 1., 0.).normalize();
+    let velocity = Vector::new(1., 1.8, 0.).normalize() * 11.25;
     let mut p = Projectile { position, velocity };
+
+    let mut canvas = Canvas::new(900, 553);
+    canvas.fill(Color::new(1.0, 1.0, 1.0));
 
     let gravity = Vector::new(0., -0.1, 0.);
     let wind = Vector::new(-0.01, 0., 0.);
     let e = Environment { gravity, wind };
 
-    println!("({}, {})", p.position.x, p.position.y);
     loop {
         p = tick(&e, p);
-        println!("({}, {})", p.position.x, p.position.y);
+
+        let x = p.position.x.max(0.) as usize;
+        let y = (550. - (p.position.y.max(0.))).min(549.) as usize;
+        canvas.write_pixel(x, y, Color::new(0.0, 0.0, 0.0));
 
         if p.position.y <= 0. {
             break;
         }
     }
+
+    canvas.to_ppm()
 }
 
-pub fn main() {
-    simulate();
+pub fn main() -> std::io::Result<()> {
+    let ppm = simulate_and_plot();
+    let mut file = File::create("plot.ppm")?;
+    file.write_all(ppm.blob().as_bytes())?;
+    Ok(())
 }
