c403bf9a0ec6f90160ab038a25a5622790cf1474
Ryan James Spencer <spencer.ryanjames@gmail.com>
2019-05-23 20:52:50 +1000

    Test for 2x2 and 3x3 matrices

--- rust/src/matrix.rs
+++ rust/src/matrix.rs
@@ -9,24 +9,15 @@ struct Matrix {
 }
 
 trait SquareMatrix {
-    fn empty(dim: usize) -> Self;
     fn from_vec(vec: Vec<f64>) -> Self;
 }
 
 impl SquareMatrix for Matrix {
-    fn empty(dim: usize) -> Self {
-        Matrix {
-            dims: (dim, dim),
-            data: smallvec![0.0; dim],
-        }
-    }
-
     // A dependantly typed language could encode this in the type system.
     // Rust is not one of those languages, so instead we'll have do the checks at runtime here.
     fn from_vec(vec: Vec<f64>) -> Self {
         let dim = (vec.len() as f64).log2() as usize;
-        assert!((dim ^ 2) % 2 == 0);
-        assert!((dim ^ 2) <= 4 * 4); // We only support 4x4 matrices and below.
+        assert!(vec.len() == 4 || vec.len() == 9 || vec.len() == 16);
         Matrix {
             dims: (dim, dim),
             data: SmallVec::from_vec(vec),
@@ -39,10 +30,9 @@ impl Index<(usize, usize)> for Matrix {
     type Output = f64;
 
     fn index(&self, ixs: (usize, usize)) -> &f64 {
+        // TODO Convert `assert`s to `Result`s.
         assert!(ixs.0 < self.dims.0);
         assert!(ixs.1 < self.dims.1);
-        println!("{}, {}", self.dims.0, self.dims.1);
-        println!("{}, {}", ixs.0, ixs.1);
         &self.data[(ixs.0 * self.dims.0) + ixs.1]
     }
 }
@@ -67,5 +57,57 @@ mod test {
         assert_eq!(m[(2, 2)], 11.0);
         assert_eq!(m[(3, 0)], 13.5);
         assert_eq!(m[(3, 2)], 15.5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn constructing_invalid_matrices_01() {
+        #[rustfmt::skip]
+        let _: Matrix = SquareMatrix::from_vec(vec![
+           1.,   2.,   3.,  4.,
+           5.5,  6.5,  7.5, 8.5,
+           9.,   10.,  11., 12.,
+           13.5, 14.5, 15.5
+        ]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn constructing_invalid_matrices_02() {
+        #[rustfmt::skip]
+        let _: Matrix = SquareMatrix::from_vec(vec![1., 2.]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn constructing_invalid_matrices_03() {
+        #[rustfmt::skip]
+        let _: Matrix = SquareMatrix::from_vec(vec![1.]);
+    }
+
+    #[test]
+    fn a_2x2_matrix_ought_to_be_representable() {
+        #[rustfmt::skip]
+        let m: Matrix = SquareMatrix::from_vec(vec![
+            -3., 5.,
+            1., -2.,
+        ]);
+        assert_eq!(m[(0, 0)], -3.);
+        assert_eq!(m[(0, 1)], 5.0);
+        assert_eq!(m[(1, 0)], 1.0);
+        assert_eq!(m[(1, 1)], -2.0);
+    }
+
+    #[test]
+    fn a_3x3_matrix_ought_to_be_representable() {
+        #[rustfmt::skip]
+        let m: Matrix = SquareMatrix::from_vec(vec![
+            -3., 5., 0.,
+            1., -2., -7.,
+            0., 1., 1.,
+        ]);
+        assert_eq!(m[(0, 0)], -3.0);
+        assert_eq!(m[(1, 1)], -2.0);
+        assert_eq!(m[(2, 2)], 1.0);
     }
 }
